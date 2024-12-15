26df8ef96a2ea60edd577d7a7612678330c21726
Md Shamim Reza <opt.shamim@gmail.com>
2024-12-15 09:48:47 +0600

    custom authorisation inital commit

--- profiles/utils.py
+++ profiles/utils.py
@@ -1,5 +1,19 @@
 from textblob import TextBlob
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from .models import University
 
 def analyze_sentiment(text):
     blob = TextBlob(text)
     return blob.sentiment  # Returns a Sentiment object (polarity, subjectivity)
+
+def create_custom_permissions():
+    # Use an existing model's ContentType (or create a new one)
+    content_type = ContentType.objects.get_for_model(University)
+    
+    # Create a custom permission for viewing universities
+    Permission.objects.get_or_create(
+        codename='view_university',
+        name='Can view university',
+        content_type=content_type,
+    )
